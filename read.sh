0af99922f754d530d3c83928bcef1631fbd69ace
Scot Brown <scotb38@gmail.com>
2018-01-25 11:45:30 -0500

    add read and write scripts

---
+++ read.sh
@@ -0,0 +1,7 @@
+#! /bin/bash
+
+cp ~/.config/i3status/config config/i3status/config
+cp ~/.config/mimeapps.list config/mimeapps.list
+cp ~/.i3/config i3/config
+cp ~/.Xresources Xresources
+cp ~/.spacemacs spacemacs
